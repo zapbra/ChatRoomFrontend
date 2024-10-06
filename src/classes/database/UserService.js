add7bb065c42cbce17ee5ded3aa0cb59f9275d8f
Matthew Pierce <lesbianhaircut@outlook.com>
2024-10-06 19:07:08 -0400

    working on desktop

--- src/classes/database/UserService.js
+++ src/classes/database/UserService.js
@@ -1,54 +1,67 @@
 export class UserService {
-  static apiUrl = process.env.REACT_APP_SERVER_BASE_URL;
+    static apiUrl = process.env.REACT_APP_SERVER_BASE_URL;
 
-  /**
-   * Fetches and returns a list of all the users of the application
-   * @returns {Array.<Object>} The list of users
-   */
-  static async getUsers() {
-    try {
-      const response = await fetch(`${this.apiUrl}/api/Users`);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return await response.json();
-    } catch (error) {
-      console.log("Error fetching users:", error);
-      throw error;
+    /**
+     * Fetches and returns a list of all the users of the application
+     * @returns {Array.<Object>} The list of users
+     */
+    static async getUsers() {
+        try {
+            const response = await fetch(`${this.apiUrl}/api/Users`);
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return await response.json();
+        } catch (error) {
+            console.log("Error fetching users:", error);
+            throw error;
+        }
     }
-  }
 
-  /**
-   *
-   * @param {string} email - The email of the user
-   * @param {string} username - The username of the user
-   * @param {string} password - The password of the user
-   */
-  static async insertUserLogin({ email, username, password, roleName }) {
-    const roleId = this.getRoleIdByName(roleName);
+    /**
+     *
+     * @param {string} email - The email of the user
+     * @param {string} username - The username of the user
+     * @param {string} password - The password of the user
+     */
+    static async signupUserLogin(email, username, password, roleName) {
+        const roleId = this.getRoleIdByName(roleName);
 
-    try {
-      const response = await fetch(`${this.apiUrl}/api/Users/Userlogins`, {
-        method: "POST",
-        body: JSON.stringify({
-          user: { roleId: roleId },
-          username,
-          password,
-          email,
-        }),
-      });
-    } catch (error) {
-      console.log("Error inserting user: ", error);
-      throw error;
+        try {
+            const response = await fetch(
+                `${this.apiUrl}/api/Users/Userlogins`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        user: { roleId: roleId },
+                        username,
+                        password,
+                        email,
+                    }),
+                }
+            );
+            if (!response.ok) {
+                console.log("response");
+                console.log(await response.json());
+
+                throw new Error("Network response was not ok");
+            }
+            return await response.json();
+        } catch (error) {
+            console.log("Error inserting user: ", error);
+            throw error;
+        }
     }
-  }
 
-  /**
-   *
-   * @param {string} roleName
-   * @returns {int} The id of the role to use for db inserts mainly
-   */
-  static getRoleIdByName(roleName) {
-    return 1;
-  }
+    /**
+     *
+     * @param {string} roleName
+     * @returns {int} The id of the role to use for db inserts mainly
+     */
+    static getRoleIdByName(roleName) {
+        return 1;
+    }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,85 +3,94 @@ import { useState } from "react";
 import { UserService } from "../classes/database/UserService";
 
 const Signup = ({ signUp }) => {
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState(false);
-  const [username, setUsername] = useState("");
-  const [usernameError, setUsernameError] = useState(false);
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
+    const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState(false);
+    const [username, setUsername] = useState("");
+    const [usernameError, setUsernameError] = useState(false);
+    const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState(false);
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    setEmailError(false);
-    setUsernameError(false);
-    setPasswordError(false);
+    async function handleSubmit(event) {
+        event.preventDefault();
+        setEmailError(false);
+        setUsernameError(false);
+        setPasswordError(false);
 
-    const users = await UserService.getUsers();
-    console.log("users", users);
-    if (email === "") {
-      setEmailError(true);
+        if (email === "") {
+            setEmailError(true);
+        }
+        if (username === "") {
+            setUsernameError(true);
+        }
+        if (password === "") {
+            setPasswordError(true);
+        }
+
+        console.log(
+            `email ${email}, username ${username}, password = ${password}`
+        );
+
+        if (email && username && password) {
+            const response = await UserService.signupUserLogin(
+                email,
+                username,
+                password,
+                "user"
+            );
+            console.log("response");
+            console.log(response);
+        }
     }
-    if (username === "") {
-      setUsernameError(true);
-    }
-    if (password === "") {
-      setPasswordError(true);
-    }
-    /*
-    if (email && username && password) {
-      console.log(email, username, password);
-    } */
-  }
 
-  return (
-    <div className="mx-auto max-w-7xl">
-      <h1 className="text-4xl mb-8">Register Form</h1>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="username"
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          variant="outlined"
-          color="secondary"
-          type="type"
-          sx={{ mb: 3 }}
-          fullWidth
-          value={username}
-          error={usernameError}
-        />
+    return (
+        <div className="mx-auto max-w-7xl">
+            <h1 className="text-4xl mb-8">Register Form</h1>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="username"
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    variant="outlined"
+                    color="secondary"
+                    type="type"
+                    sx={{ mb: 3 }}
+                    fullWidth
+                    value={username}
+                    error={usernameError}
+                />
 
-        <TextField
-          label="email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          variant="outlined"
-          color="secondary"
-          type="email"
-          sx={{ mb: 3 }}
-          fullWidth
-          value={email}
-          error={emailError}
-        />
+                <TextField
+                    label="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    variant="outlined"
+                    color="secondary"
+                    type="email"
+                    sx={{ mb: 3 }}
+                    fullWidth
+                    value={email}
+                    error={emailError}
+                />
 
-        <TextField
-          label="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          variant="outlined"
-          color="secondary"
-          type="password"
-          sx={{ mb: 3 }}
-          fullWidth
-          value={password}
-          error={passwordError}
-        />
+                <TextField
+                    label="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    variant="outlined"
+                    color="secondary"
+                    type="password"
+                    sx={{ mb: 3 }}
+                    fullWidth
+                    value={password}
+                    error={passwordError}
+                />
 
-        <Button variant="outlined" color="secondary" type="submit">
-          Register
-        </Button>
-      </form>
-    </div>
-  );
+                <Button variant="outlined" color="secondary" type="submit">
+                    Register
+                </Button>
+            </form>
+        </div>
+    );
 };
 
 export default Signup;
